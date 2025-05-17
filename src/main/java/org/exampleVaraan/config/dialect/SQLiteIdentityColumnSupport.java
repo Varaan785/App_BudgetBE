723038f58065bb099f5ece8dad7a2e7f7a918dd3
varaan785@gmail.com <156232089+Varaan785@users.noreply.github.com>
2025-05-17 17:55:47 +0200

    UPDATE 17.05.2025

--- src/main/java/org/exampleVaraan/config/dialect/SQLiteIdentityColumnSupport.java
+++
@@ -1,7 +0,0 @@
-package org.exampleVaraan.config.dialect;
-
-public interface SQLiteIdentityColumnSupport {
-    String getIdentitySelectString(String table, String column);
-
-    String getIdentityColumnString();
-}
---
+++ src/main/java/org/exampleVaraan/config/dialect/SQLiteIdentityColumnSupportImpl.java
@@ -0,0 +1,21 @@
+package org.exampleVaraan.config.dialect;
+
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
+
+public class SQLiteIdentityColumnSupportImpl extends IdentityColumnSupportImpl {
+
+    @Override
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    @Override
+    public String getIdentitySelectString(String table, String column, int type) {
+        return "select last_insert_rowid()";
+    }
+
+    @Override
+    public String getIdentityColumnString(int type) {
+        return "integer primary key autoincrement";
+    }
+}
